b2b6153996e99d38cdce85207c6383ffa76ba92e
Kangmin <mk@kangmin.kr>
2023-01-22 15:21:15 +0900

    products CRUD완성

--- api/controller/productController.js
+++ api/controller/productController.js
@@ -2,42 +2,63 @@ const recordsPerPage = require("../config/pagination");
 const Product = require("../models/ProductModel");
 
 const getProducts = async (req, res, next) => {
-    try {
+  try {
+    let query = {};
+    let queryCondition = false;
 
-        let query = {}
-        let queryCondition = false
+    let priceQueryCondition = {};
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
 
-        let proiceQueryCondition ={}
-        if (req.query.price) {
-            queryCondition = true
-            proiceQueryCondition = { price: { $lte: Number(req.query.price)}}
-        }
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
 
-        let ratingQueryCondition = {}
-        if (req.query.rating) {
-            queryCondition = true
-            ratingQueryCondition = { rating: { $in: req.query.rating.split(',')}}
-        }
-        
-        if (queryCondition) {
-            query = {
-                $and: [proiceQueryCondition, ratingQueryCondition]
-            }
-        }
+    // get Products by category (searchBar 방식 (직접URL에 입력))
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
+    }
 
-      
-    // Pagination    
+    // get Products by category (쿼리 params에서 가져오기)
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if (item) return new RegExp("^" + item);
+      });
+      categoryQueryCondition = { category: { $in: a } };
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+        ],
+      };
+    }
+
+    // Pagination
     const pageNum = Number(req.query.pageNum) || 1;
-    
+
     //sort by name, price etc...
     let sort = {};
     const sortOption = req.query.sort || "";
     if (sortOption) {
-        let sortOpt = sortOption.split("_");
-        sort = { [sortOpt[0]]: Number(sortOpt[1]) };
-        console.log(sort);
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+      console.log(sort);
     }
-    
+
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
@@ -53,4 +74,116 @@ const getProducts = async (req, res, next) => {
   }
 };
 
-module.exports = getProducts;
+const getProductById = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id)
+      .populate("reviews")
+      .orFail();
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getBestsellers = async (req, res, next) => {
+  try {
+    const products = await Product.aggregate([
+      { $sort: { category: 1, sales: -1 } },
+      {
+        $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } },
+      },
+      { $replaceWith: "$doc_with_max_sales" },
+      { $match: { sales: { $gt: 0 } } },
+      { $project: { _id: 1, name: 1, image: 1, category: 1, description: 1 } },
+      { $limit: 3 },
+    ]);
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminGetProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find({})
+      .sort({ category: 1 })
+      .select("name price category");
+
+    return res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminDeleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).orFail();
+    await product.remove();
+    res.json({ message: "Product removed successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminCreteProduct = async (req, res, next) => {
+  try {
+    const product = new Product();
+    const { name, description, category, count, price, attributesTable } =
+      req.body;
+    product.name = name;
+    product.description = description;
+    product.category = category;
+    product.count = count;
+    product.price = price;
+    if (attributesTable.length > 0) {
+      product.attrs = [];
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    }
+    await product.save();
+    res.json({
+      message: "Product created",
+      productId: product._id,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminUpdateProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).orFail();
+    const { name, description, category, count, price, attributesTable } =
+      req.body;
+    product.name = name || product.name;
+    product.description = description || product.description;
+    product.category = category || product.category;
+    product.count = count || product.count;
+    product.price = price || product.price;
+    if (attributesTable.length > 0) {
+      product.attrs = [];
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    } else {
+      product.attrs = [];
+    }
+      
+    await product.save();
+    res.json({ message: "Product Updated" });
+      
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  getBestsellers,
+  adminGetProducts,
+  adminDeleteProduct,
+  adminCreteProduct,
+  adminUpdateProduct,
+};
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const router = express.Router()
-const getProducts = require('../controller/productController')
+const express = require("express");
+const router = express.Router();
+const {
+  getProducts,
+  getProductById,
+  getBestsellers,
+  adminGetProducts,
+  adminDeleteProduct,
+  adminUpdateProduct,
+  adminCreteProduct,
+} = require("../controller/productController");
 
-router.get("/", getProducts)
+router.get("/category/:categoryName", getProducts);
 
+router.get("/", getProducts);
 
+router.get("/bestsellers", getBestsellers);
 
-module.exports = router
+router.get("/get-one/:id", getProductById);
+
+//admin routes:
+
+router.get("/admin", adminGetProducts);
+router.delete("/admin/:id", adminDeleteProduct);
+router.put("/admin/:id", adminUpdateProduct);
+router.post("/admin", adminCreteProduct);
+
+module.exports = router;
