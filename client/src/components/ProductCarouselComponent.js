3a60d545b90f72c14dd4782e610bdb059b2611fb
Kangmin <mk@kangmin.kr>
2023-01-25 02:02:19 +0900

    mainpage card작성

--- client/src/components/ProductCarouselComponent.js
+++ client/src/components/ProductCarouselComponent.js
@@ -1,4 +1,5 @@
 import { Carousel } from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap"
 
 const ProductCarouselComponent = () => {
   return (
@@ -6,40 +7,47 @@ const ProductCarouselComponent = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{height:"300px" ,objectFit:"cover"}}
           src="/images/carousel/carousel-1.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <LinkContainer style={{cursor:"pointer",color:"yellow"}} to="/product-details">
+            <h3>Bestseller in Notebooks Category</h3>
+          </LinkContainer>
+          <p>Dell Inspiron 15 3000, 15.6 inch HD</p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{height:"300px" ,objectFit:"cover"}}
           src="/images/carousel/carousel-2.png"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <LinkContainer style={{cursor:"pointer",color:"yellow"}} to="/product-details">
+            <h3>Bestseller in Books Category</h3>
+          </LinkContainer>
+          <p>정춘희 , 흐르는 강물에 꽃을 띄우라</p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{height:"300px" ,objectFit:"cover"}}
           src="/images/carousel/carousel-3.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+        <LinkContainer style={{cursor:"pointer",color:"yellow"}} to="/product-details">
+            <h3>Bestseller in Cameras Category</h3>
+          </LinkContainer>
+          <p>Canon, V7 Full Frame</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
