a82c1bc61fde5a95ba0db6dbe26c1a4b8796d61a
Kangmin <mk@kangmin.kr>
2023-01-30 19:24:51 +0900

    server & react base 완성

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,6 +1,5 @@
 const Product = require("../models/ProductModel");
 const recordsPerPage = require("../config/pagination");
-const imageValidate = require("../utils/imageValidate");
 
 const getProducts = async (req, res, next) => {
   try {
@@ -12,14 +11,11 @@ const getProducts = async (req, res, next) => {
       queryCondition = true;
       priceQueryCondition = { price: { $lte: Number(req.query.price) } };
     }
-
     let ratingQueryCondition = {};
     if (req.query.rating) {
       queryCondition = true;
       ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
     }
-
-    // get Products by category (searchBar 방식 (직접URL에 입력))
     let categoryQueryCondition = {};
     const categoryName = req.params.categoryName || "";
     if (categoryName) {
@@ -28,14 +24,57 @@ const getProducts = async (req, res, next) => {
       var regEx = new RegExp("^" + a);
       categoryQueryCondition = { category: regEx };
     }
-
-    // get Products by category (쿼리 params에서 가져오기)
     if (req.query.category) {
       queryCondition = true;
       let a = req.query.category.split(",").map((item) => {
         if (item) return new RegExp("^" + item);
       });
-      categoryQueryCondition = { category: { $in: a } };
+      categoryQueryCondition = {
+        category: { $in: a },
+      };
+    }
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      // attrs=RAM-1TB-2TB-4TB,color-blue-red
+      // [ 'RAM-1TB-4TB', 'color-blue', '' ]
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          let a = item.split("-");
+          let values = [...a];
+          values.shift(); // removes first item
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          acc.push(a1);
+          // console.dir(acc, { depth: null })
+          return acc;
+        } else return acc;
+      }, []);
+    //   console.dir(attrsQueryCondition, { depth: null });
+    queryCondition = true;
+    }
+
+    //pagination
+    const pageNum = Number(req.query.pageNum) || 1;
+
+    // sort by name, price etc.
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+    }
+
+    const searchQuery = req.params.searchQuery || ""
+    let searchQueryCondition = {}
+    let select = {}
+    if(searchQuery) {
+        queryCondition = true
+        searchQueryCondition = { $text: { $search: searchQuery } }
+        select = {
+            score: { $meta: "textScore" }
+        }
+        sort = { score: { $meta: "textScore" } }
     }
 
     if (queryCondition) {
@@ -44,218 +83,27 @@ const getProducts = async (req, res, next) => {
           priceQueryCondition,
           ratingQueryCondition,
           categoryQueryCondition,
+          searchQueryCondition,
+          ...attrsQueryCondition
         ],
       };
     }
 
-    // Pagination
-    const pageNum = Number(req.query.pageNum) || 1;
-
-    //sort by name, price etc...
-    let sort = {};
-    const sortOption = req.query.sort || "";
-    if (sortOption) {
-      let sortOpt = sortOption.split("_");
-      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
-      console.log(sort);
-    }
-
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
+        .select(select)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
+
     res.json({
       products,
       pageNum,
       paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
     });
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    next(error);
   }
 };
+module.exports = getProducts;
 
-const getProductById = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id)
-      .populate("reviews")
-      .orFail();
-    res.json(product);
-  } catch (err) {
-    next(err);
-  }
-};
-
-const getBestsellers = async (req, res, next) => {
-  try {
-    const products = await Product.aggregate([
-      { $sort: { category: 1, sales: -1 } },
-      {
-        $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } },
-      },
-      { $replaceWith: "$doc_with_max_sales" },
-      { $match: { sales: { $gt: 0 } } },
-      { $project: { _id: 1, name: 1, image: 1, category: 1, description: 1 } },
-      { $limit: 3 },
-    ]);
-    res.json(products);
-  } catch (err) {
-    next(err); 
-  } 
-};
-
-const adminGetProducts = async (req, res, next) => {
-  try {
-    const products = await Product.find({})
-      .sort({ category: 1 })
-      .select("name price category");
-
-    return res.json(products);
-  } catch (err) {
-    next(err);
-  }
-};
-
-const adminDeleteProduct = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id).orFail();
-    await product.remove();
-    res.json({ message: "Product removed successfully" });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const adminCreateProduct = async (req, res, next) => {
-  try {
-    const product = new Product();
-    const { name, description, category, count, price, attributesTable } =
-      req.body;
-    product.name = name;
-    product.description = description;
-    product.category = category;
-    product.count = count;
-    product.price = price;
-    if (attributesTable.length > 0) {
-      product.attrs = [];
-      attributesTable.map((item) => {
-        product.attrs.push(item);
-      });
-    }
-    await product.save();
-    res.json({
-      message: "Product created",
-      productId: product._id,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const adminUpdateProduct = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id).orFail();
-    const { name, description, category, count, price, attributesTable } =
-      req.body;
-    product.name = name || product.name;
-    product.description = description || product.description;
-    product.category = category || product.category;
-    product.count = count || product.count;
-    product.price = price || product.price;
-    if (attributesTable.length > 0) {
-      product.attrs = [];
-      attributesTable.map((item) => {
-        product.attrs.push(item);
-      });
-    } else {
-      product.attrs = [];
-    }
-
-    await product.save();
-    res.json({ message: "Product Updated" });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const adminUpload = async (req, res, next) => {
-  try {
-    if (!req.files || !!req.files.images === false) {
-      return res.status(400).send("No files were uploaded.");
-    }
-
-    const validateResult = imageValidate(req.files.images);
-    if (validateResult.error) {
-      return res.status(400).send(validateResult.error);
-    }
-
-    const path = require("path");
-    const { v4: uuidv4 } = require("uuid");
-    const uploadDirectory = path.resolve(
-      __dirname,
-      "../../client",
-      "public",
-      "images",
-      "products"
-    );
-
-    let product = await Product.findById(req.query.productId).orFail();
-
-    let imagesTable = [];
-    if (Array.isArray(req.files.images)) {
-      imagesTable = req.files.images;
-    } else {
-      imagesTable.push(req.files.images);
-    }
-
-    for (let image of imagesTable) {
-      var fileName = uuidv4() + path.extname(image.name);
-      var uploadPath = uploadDirectory + "/" + fileName;
-      product.images.push({ path: "/images/products/" + fileName });
-      image.mv(uploadPath, function (err) {
-        if (err) {
-          return res.status(500).send(err);
-        }
-      });
-    }
-    await product.save();
-    return res.send("Files uploaded!");
-  } catch (err) {
-    next(err);
-  }
-};
-
-const adminDeleteProductImage = async (req, res, next) => {
-  try {
-    const imagePath = decodeURIComponent(req.params.imagePath);
-
-    const path = require("path");
-    const finalPath = path.resolve("../client/public") + imagePath;
-    const fs = require("fs")  
-      fs.unlink(finalPath, (err) => {
-          if (err) {
-            res.status(500).send(err);
-        }
-    })    
-      await Product.findOneAndUpdate(
-          { _id: req.params.productId },
-          { $pull: { images: { path: imagePath } } }).orFail()
-    return res.end()
-      
-  } catch (err) {
-    next(err);
-  }
-
-};
-
-module.exports = {
-  getProducts,
-  getProductById,
-  getBestsellers,
-  adminGetProducts,
-  adminDeleteProduct,
-  adminCreateProduct,
-  adminUpdateProduct,
-  adminUpload,
-  adminDeleteProductImage,
-};
